462d3893e08a2145fd3d733917bb8d0c599d0f3c
Antonio Soardi <40772919+antosoa@users.noreply.github.com>
2020-05-15 10:48:26 +0200

    add string algo 02

---
+++ withoutString.java
@@ -0,0 +1,14 @@
+public String withoutString(String base, String remove) {
+ //Given two strings, base and remove, 
+ //return a version of the base string 
+ //where all instances of the remove string 
+ //have been removed (not case sensitive). 
+ //You may assume that the remove string is 
+ //length 1 or more. Remove only non-overlapping instances, 
+ //so with "xxx" removing "xx" leaves "x".
+  
+  base = base.replaceAll(remove.toLowerCase(), "");
+  base = base.replaceAll(remove.toUpperCase(), "");
+  return base.replaceAll(remove, "");
+  
+}
